bbce6c3ea198a0cc77030c053593147857b899bc
Deniz <denvanchiba@gmail.com>
2025-04-16 05:01:33 +0300

    create: Project Template

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+
+const ibmPlexSans = IBM_Plex_Sans({
+  variable: "--font-main",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: "Banksan",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${ibmPlexSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
