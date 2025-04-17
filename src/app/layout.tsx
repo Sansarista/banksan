680f61ab42d03da272e1696596fe458652e3e98c
Deniz <denvanchiba@gmail.com>
2025-04-17 03:29:49 +0300

    update: new fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,19 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Sans } from "next/font/google";
+import { IBM_Plex_Sans, Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/Header";
 
 const ibmPlexSans = IBM_Plex_Sans({
   variable: "--font-main",
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
+  display: 'swap',
+});
+
+const bricolageGrotesque = Bricolage_Grotesque({
+  variable: "--font-logo",
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
   display: 'swap',
 });
 
@@ -21,8 +29,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${ibmPlexSans.variable} antialiased`}
+        className={`${ibmPlexSans.variable} ${bricolageGrotesque.variable} relative antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
