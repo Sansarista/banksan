e92d8f9794b65c2b85066e5c489d4f946e9aadfd
Deniz <denvanchiba@gmail.com>
2025-04-17 05:28:09 +0300

    Update: Motto section added to Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/sections/Hero"
 import Stats from "@/components/sections/Stats"
-
+import Motto from "@/components/sections/Motto"
 export default function Home() {
   return (
     <>
           <Hero/>
           <Stats/>
+          <Motto/>
     </>
   )
 }
