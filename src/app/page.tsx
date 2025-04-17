3e50b34d3bbbf4ea7bef962a80c6844739291dec
Deniz <denvanchiba@gmail.com>
2025-04-17 21:01:12 +0300

    Update: Updated Main Page for New Sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,18 @@ import Stats from "@/components/sections/Stats"
 import Motto from "@/components/sections/Motto"
 import Privileges from "@/components/sections/Privileges"
 import Reviews from "@/components/sections/Reviews"
+import Features from "@/components/sections/Features"
+import CTA from "@/components/sections/CTA"
+
 export default function Home() {
   return (
     <>
           <Hero/>
           <Stats/>
-          <Motto/>
+          <Features/>
           <Privileges/>
           <Reviews/>
+          <CTA/>
     </>
   )
 }
