462150e16331beba3b3b31b7e79b9d40159e983f
Deniz <denvanchiba@gmail.com>
2025-04-17 20:47:56 +0300

    Update:  Reviews section added to Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/sections/Hero"
 import Stats from "@/components/sections/Stats"
 import Motto from "@/components/sections/Motto"
 import Privileges from "@/components/sections/Privileges"
-
+import Reviews from "@/components/sections/Reviews"
 export default function Home() {
   return (
     <>
@@ -10,6 +10,7 @@ export default function Home() {
           <Stats/>
           <Motto/>
           <Privileges/>
+          <Reviews/>
     </>
   )
 }
