c4b50996d3af879f4e3082aacd98e534f1ebfccd
Deniz <denvanchiba@gmail.com>
2025-04-17 21:09:49 +0300

    Update: Removed Motto seciton from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/sections/Hero"
 import Stats from "@/components/sections/Stats"
-import Motto from "@/components/sections/Motto"
 import Privileges from "@/components/sections/Privileges"
 import Reviews from "@/components/sections/Reviews"
 import Features from "@/components/sections/Features"
