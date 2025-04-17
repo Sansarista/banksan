69e112363adc015ebd2e3c08f6dc1a4049798046
Deniz <denvanchiba@gmail.com>
2025-04-17 05:49:30 +0300

    Update: Added CSS classes to Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <head>
         <link rel="icon" href="/b-cropped.svg" sizes="any" />
       </head>
