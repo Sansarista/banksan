20ddbac019cfb75aee627f2dbc01f4dd0cd334da
Deniz <denvanchiba@gmail.com>
2025-04-17 17:26:48 +0300

    Update: Privileges section added to Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Hero from "@/components/sections/Hero"
 import Stats from "@/components/sections/Stats"
 import Motto from "@/components/sections/Motto"
+import Privileges from "@/components/sections/Privileges"
+
 export default function Home() {
   return (
     <>
           <Hero/>
           <Stats/>
           <Motto/>
+          <Privileges/>
     </>
   )
 }
